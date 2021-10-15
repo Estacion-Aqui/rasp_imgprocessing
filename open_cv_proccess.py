bce2a15d0a44dae0c6f5671fc4c91b4de6342ef3
icrxz <igor.cruz@mercadolivre.com>
2021-10-15 09:47:48 -0300

    test new algorithm

--- open_cv_proccess.py
+++ open_cv_proccess.py
@@ -1,50 +1,167 @@
 import cv2, os, pytesseract
 from pathlib import Path
+import numpy as np
 
 
 
 class OpenCvProcess:
 
-  pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files (x86)\Tesseract-OCR\tesseract.exe"
-
   imageSource = ""
+  min_area = 4500
+  max_area = 30000
+  
+  def preProccessImage(self, image):
+    
+    imgBlurred = cv2.GaussianBlur(image, (7, 7), 0)
 
-  def findLicensePlate(self):
-    img = cv2.imread(self.imageSource)
-    print(self.imageSource)
-
-    grey = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-    # cv2.imshow("grey", grey)
-    # cv2.waitKey(0)
-
-    _, bin = cv2.threshold(grey, 90, 255, cv2.THRESH_BINARY)
-    cv2.imshow("binary", bin)
-    cv2.waitKey(0)
-
-    # blurred = cv2.GaussianBlur(bin, (5, 5), 0)
-    # cv2.imshow("blurred", blurred)
-    # cv2.waitKey(0)
-
-    contours, hierarchy = cv2.findContours(bin, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
-
-    for c in contours:
-      perimeter = cv2.arcLength(c, True)
-      print("teste contorno")
-      if perimeter > 200:
-        print("teste perimetro")
-        approx = cv2.approxPolyDP(c, 0.025 * perimeter, True)
-        if len(approx) == 4:
-          (x, y, alt, lar) = cv2.boundingRect(c)
-          cv2.rectangle(img, (x, y), (x + alt, y + lar), (255, 0, 0), 2)
-          roi = img[y:y + lar, x:x + alt]
-          cv2.imshow("roi", roi)
-          cv2.waitKey(0)
-          cv2.imwrite('out/roi.png', roi)
-
-    cv2.imshow("contours", img)
+    grey = cv2.cvtColor(imgBlurred, cv2.COLOR_BGR2GRAY)
+    
+    sobelx = cv2.Sobel(grey, cv2.CV_8U, 1, 0, ksize = 3)
+    
+    _, threshold_img = cv2.threshold(sobelx, 0, 255, cv2.THRESH_BINARY + cv2.THRESH_OTSU)
+    
+    element = cv2.getStructuringElement(shape = cv2.MORPH_RECT, ksize =(22, 3))
+    morph_n_thresholded_img = threshold_img.copy()
+    
+    cv2.morphologyEx(src = threshold_img,
+      op = cv2.MORPH_CLOSE,
+      kernel = element,
+      dst = morph_n_thresholded_img)
+    
+    cv2.imshow("threshold", threshold_img)
     cv2.waitKey(0)
     cv2.destroyAllWindows()
+		
+    return morph_n_thresholded_img
+  
+  def preRatioCheck(self, area, width, height):
+		
+    min = self.min_area
+    max = self.max_area
 
+    ratioMin = 2.5
+    ratioMax = 7
+
+    ratio = float(width) / float(height)
+		
+    if ratio < 1:
+      ratio = 1 / ratio
+
+    if (area < min or area > max) or (ratio < ratioMin or ratio > ratioMax):
+      return False
+		
+    return True
+  
+  def validateRatio(self, rect):
+    (x, y), (width, height), rect_angle = rect
+
+    if (width > height):
+    	angle = -rect_angle
+    else:
+    	angle = 90 + rect_angle
+
+    if angle > 15:
+    	return False
+		
+    if (height == 0 or width == 0):
+    	return False
+
+    area = width * height
+		
+    if not self.preRatioCheck(area, width, height):
+    	return False
+    else:
+    	return True
+   
+  def ratioCheck(self, area, width, height):
+		
+    min = self.min_area
+    max = self.max_area
+
+    ratioMin = 3
+    ratioMax = 6
+
+    ratio = float(width) / float(height)
+		
+    if ratio < 1:
+      ratio = 1 / ratio
+		
+    if (area < min or area > max) or (ratio < ratioMin or ratio > ratioMax):
+      return False
+		
+    return True
+   
+  def clean_plate(self, plate):
+		
+    gray = cv2.cvtColor(plate, cv2.COLOR_BGR2GRAY)
+    thresh = cv2.adaptiveThreshold(gray, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 2)
+		
+    _, contours, _ = cv2.findContours(thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    if contours:
+      areas = [cv2.contourArea(c) for c in contours]
+			
+			# index of the largest contour in the area
+			# array
+      max_index = np.argmax(areas)
+
+      max_cnt = contours[max_index]
+      max_cntArea = areas[max_index]
+      x, y, w, h = cv2.boundingRect(max_cnt)
+      rect = cv2.minAreaRect(max_cnt)
+			
+      if not self.ratioCheck(max_cntArea, plate.shape[1], plate.shape[0]):
+        return plate, False, None
+			
+      return plate, True, [x, y, w, h]
+		
+    else:
+      return plate, False, None
+  
+  def checkPlate(self, image, contour):
+    
+    min_rect = cv2.minAreaRect(contour)
+    
+    if self.validateRatio(min_rect):
+      x, y, w, h = cv2.boundingRect(contour)
+      after_validation_img = image[y:y + h, x:x + w]
+      after_clean_plate_img, plateFound, coordinates = self.clean_plate(after_validation_img)
+      
+      if plateFound:
+          x1, y1, w1, h1 = coordinates
+          coordinates = x1 + x, y1 + y
+          
+          return after_clean_plate_img, coordinates
+    
+    return None, None
+    
+
+  def findLicensePlate(self):
+    
+    inputImage = cv2.imread(self.imageSource)
+    plates = []
+    self.corresponding_area = []
+    
+    processedImage = self.preProccessImage(inputImage)
+
+    contours, _ = cv2.findContours(processedImage, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_NONE)
+
+    for cnt in contours:
+      plateImage, coordinates = self.checkPlate(inputImage, cnt)
+      
+      if plateImage is not None:
+        plates.append(plateImage)
+        self.corresponding_area.append(coordinates)
+        cv2.imshow("roi", plateImage)
+        cv2.waitKey(0)
+        cv2.imwrite('out/roi.png', plateImage)
+        
+    if (len(plates) > 0):
+      return plates
+    
+    else:
+      return None
+    
 
   def preProcessPlateImage(self):
     img_roi = cv2.imread("out/roi.png")
@@ -92,7 +209,7 @@ class OpenCvProcess:
   def exec(self, imgSource):
     self.imageSource = imgSource
 
-    self.findLicensePlate()
+    plates = self.findLicensePlate()
     self.preProcessPlateImage()
     plateChars = self.ocrPlateImage()
 
